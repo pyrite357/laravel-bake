5ea7d5103862f04d3a8c8d8e153c210a52844384
Brandon Tanner <brandon.tanner@radeuslabs.com>
2025-06-05 23:23:28 +0000

    Adding list command, not implemented though

---
+++ src/Commands/ListCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Pyrite357\LaravelBake\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+
+class ListCommand extends Command {
+
+    protected $signature = 'cake:list';
+    protected $description = 'List all tables in all schemas in the database';
+
+    public function handle() {
+
+        $tables = DB::selectAll(
+            'SELECT schema, table FROM information_schema.tables',
+            ['schema' => $schema, 'table' => $table]
+        );
+        if (!$exists) {
+            $this->error("Table '$schema.$table' does not exist.");
+            return Command::FAILURE;
+        }
+
+        print_r($tables);
+
+        return Command::SUCCESS;
+    }
+}
+
--- src/LaravelBakeServiceProvider.php
+++ src/LaravelBakeServiceProvider.php
@@ -4,19 +4,15 @@ namespace Pyrite357\LaravelBake;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelBakeServiceProvider extends ServiceProvider
-{
-    public function register()
-    {
+class LaravelBakeServiceProvider extends ServiceProvider {
+
+    public function register() {
         // Register commands
         $this->commands([
             \Pyrite357\LaravelBake\Commands\BakeCommand::class,
+            \Pyrite357\LaravelBake\Commands\ListCommand::class,
         ]);
     }
 
-    public function boot()
-    {
-        //
-    }
 }
 
